f6b58ebbf79fa535c6bd223eda65fc56f143be83
Borislav Dermendzhiev <lenamarltd@gmail.com>
2017-02-13 16:24:08 +0200

    Added Count Letters and Count Numbers

---
+++ CountLetters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    class CountLetters
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int[] countsHolder = new int[26];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    countsHolder[text[i] - 'a']++;
+                }
+            }
+
+            for (int i = 0; i < countsHolder.Length; i++)
+            {
+                if(countsHolder[i] > 0)
+                {
+                    Console.WriteLine("{0} -> {1}", (char)(i + 'a'), countsHolder[i]);
+                }
+                
+            }
+        }
+    }
+}
---
+++ CountNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNumbers
+{
+    class CountNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            list.Sort();
+            int counter = 1;
+            int previous = list[0];
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (previous == list[i])
+                {
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine("{0} -> {1}", previous, counter);
+                    counter = 1;
+                }
+
+                previous = list[i];
+
+            }
+
+            Console.WriteLine("{0} -> {1}", previous, counter);
+        }
+    }
+    }
+}
